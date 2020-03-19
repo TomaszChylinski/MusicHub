ba7d70c170d01d2a15a1920b08699a30a05aa4c2
chylinski.t <chylinski.t@outlook.com>
2020-03-19 00:03:27 -0400

    created navigation component

---
+++ client/src/component/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navigation.css";
+
+const Navigation = () => (
+
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+          <h1>Music Hub</h1>
+      </div>
+    </div>
+  );
+
+
+export default Navigation;
