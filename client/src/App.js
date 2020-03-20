6e928f2d5c970d047e2a2a56835d0ac0c8eebf9a
chylinski.t <chylinski.t@outlook.com>
2020-03-20 00:11:52 -0400

    create Home component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Navigation from "./component/Navigation";
 import Login from "./component/Login";
+import Home from "./component/Home";
 
 class App extends Component {
   render() {
@@ -11,6 +12,8 @@ class App extends Component {
         <div className="container-fluid mainContainer">
           {<Navigation />}
           {<Login />}
+
+          {<Home />}
 
           {/* <Switch>
             <Route exact path="/" component={Book} />
---
+++ client/src/component/Home/Home.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => (
+  
+  <div class="container">
+    <div class="row">
+      <div class="col-sm">
+        One of three columns
+    </div>
+      <div class="col-sm">
+        <form><div class="sc-dUjcNx begbpc">
+
+          <div size="40" class="sc-jAaTju eNQpkr"><svg xmlns="http://www.w3.org/2000/svg" width="40px" viewBox="0 0 53 53">
+            <path d="" fill="#e7eced"></path>
+
+            <path d="" fill="#757575"></path></svg></div><textarea type="textarea" name="title" placeholder="Add a post" class=""></textarea>
+
+          <input name="image" type="file" id="post-image" accept="image/x-png,image/jpeg" class="sc-krvtoX iBOgSY"></input>
+
+          <label for="post-image" class="sc-fYiAbW jkDIhX"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="20">
+
+            <path d="" fill="rgba(0, 0, 0, 0.54)"></path></svg></label></div></form>
+      </div>
+      <div class="col-sm">
+        One of three columns
+    </div>
+    </div>
+  </div>
+
+
+);
+
+
+export default Home;
